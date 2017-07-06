b0c75f397fbc7155958e06db617ad8ca3e894cda
pablosilvab <pablonicolassilvabravo@gmail.com>
2017-07-05 22:44:25 -0400

    Login JWT

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@
 
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
+var jwt = require('../services/jwt');
 
 function pruebas(req, res){
   res.status('200').send({
@@ -49,7 +50,46 @@ function saveUser(req,res){
   }
 }
 
+function loginUser(req, res){
+  var params = req.body;
+
+  var email = params.email;
+  var password = params.password;
+
+  User.findOne({email: email.toLowerCase()}, (err, user) => {
+    if (err){
+      res.status(500).send({messagge: 'Error en la petición'});
+    } else {
+      if (!user) {
+        res.status(404).send({messagge: 'Usuario no existe'});
+      } else {
+        bcrypt.compare(password, user.password, (err, check) => {
+          if (check){
+            //  Devolver datos del usuario logueado
+            if (params.gethash){
+              //  Devolver un token de JWT
+
+               res.status(200).send({
+                 token: jwt.createToken(user)
+               });
+            } else {
+              res.status(200).send({user});
+            }
+          } else {
+            //  Error 404: Contraseña incorrecta
+            res.status(404).send({messagge: 'Usuario no ha podido loguearse'});
+          }
+        });
+      }
+    }
+  });
+
+
+}
+
+
 module.exports = {
   pruebas,
-  saveUser
+  saveUser,
+  loginUser
 };
