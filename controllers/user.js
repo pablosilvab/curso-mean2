347c7141328b9e133ef665117149da624db71ed4
pablosilvab <pablonicolassilvabravo@gmail.com>
2017-07-05 22:01:43 -0400

    Registro de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,55 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
+
 function pruebas(req, res){
   res.status('200').send({
     messagge: 'Probando acción del controlador de usuarios'
   });
 }
 
+function saveUser(req,res){
+  var user = new User();
+
+  var params = req.body;
+
+  console.log(params);
+
+  user.name = params.name;
+  user.surname = params.surname;
+  user.email = params.email;
+  user.role = 'ROLE_USER';
+  user.image = 'null';
+
+  if (params.password){
+    //  Encriptar contraseña 
+    bcrypt.hash(params.password, null, null,function(err, hash){
+      user.password = hash;
+      if ( user.name != null && user.surname != null && user.email != null){
+        // Guardar usuario
+        user.save((err, userStored) => {
+          if (err){
+            res.status(500).send({messagge: 'Error al guardar usuario'});
+          } else {
+            if (!userStored){
+              res.status(404).send({messagge: 'No se ha registrado el usuario'});
+            } else {  
+              res.status(200).send({user: userStored});
+            }
+          }
+        });
+      } else {
+        res.status(200).send({messagge: 'Introduce todos los campos'});
+      }
+    });
+  } else {
+    //  Error 200
+    res.status(200).send({messagge: 'Introduce la contraseña'});
+  }
+}
+
 module.exports = {
-  pruebas
+  pruebas,
+  saveUser
 };
--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ var UserController = require('../controllers/user');
 var api = express.Router();
 
 api.get('/test-controller', UserController.pruebas);
+api.post('/register', UserController.saveUser);
 
 module.exports = api;
